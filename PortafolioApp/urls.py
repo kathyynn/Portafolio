7d4b2a1b425a23b7ec4dd7435409831a71b07024
kaathynn <137004144+kaathynn@users.noreply.github.com>
2024-06-20 16:41:59 -0400

    0.0.2

--- PortafolioApp/urls.py
+++ PortafolioApp/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from PortafolioApp.views import renderHome, renderLogin, renderRegister, renderEncuestas, renderProductos
+from PortafolioApp.views import renderHome, renderLogin, renderRegister, renderEncuestas, renderProductos, renderCampanas, renderContacto, renderQuienes, renderFunciona, renderSalud, renderViviendas, renderDiscapacitados, renderCoaniquem, renderCatastrofes, renderAnimales
 
 urlpatterns = [
     path('', renderHome),
@@ -8,5 +8,18 @@ urlpatterns = [
     path('login', renderLogin),
     path('register', renderRegister),
     path('encuestas', renderEncuestas),
-    path('productos', renderProductos)
+    path('productos', renderProductos),
+    path('campanas', renderCampanas),
+    path('contacto', renderContacto),
+    path('quienes', renderQuienes),
+    path('funciona', renderFunciona),
+    path('salud', renderSalud),
+    path('animales', renderAnimales),
+    path('catastrofes', renderCatastrofes),
+    path('coaniquem', renderCoaniquem),
+    path('discapacitados', renderDiscapacitados),
+    path('viviendas', renderViviendas)
+
+
+
 ]
--- PortafolioApp/views.py
+++ PortafolioApp/views.py
@@ -16,3 +16,33 @@ def renderEncuestas(request):
 
 def renderProductos(request):
     return render(request, 'PortafolioApp/productos.html')
+
+def renderContacto(request):
+    return render(request, 'PortafolioApp/contacto.html')
+
+def renderCampanas(request):
+    return render(request, 'PortafolioApp/campanas.html')
+
+def renderQuienes(request):
+    return render(request, 'PortafolioApp/quienes.html')
+
+def renderFunciona(request):
+    return render(request, 'PortafolioApp/funciona.html')
+
+def renderSalud(request):
+    return render(request, 'PortafolioApp/salud.html')
+
+def renderViviendas(request):
+    return render(request, 'PortafolioApp/viviendas.html')
+
+def renderAnimales(request):
+    return render(request, 'PortafolioApp/animales.html')
+
+def renderCatastrofes(request):
+    return render(request, 'PortafolioApp/catastrofes.html')
+
+def renderCoaniquem(request):
+    return render(request, 'PortafolioApp/Coaniquem.html')
+
+def renderDiscapacitados(request):
+    return render(request, 'PortafolioApp/Discapacitados.html')
